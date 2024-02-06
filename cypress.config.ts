93cbe368b56300c1b16368c04a5946faa0b56e38
DRob97 <dylanlrobertson@gmail.com>
2024-02-06 17:56:39 -0600

    updated some stuff

--- cypress.config.ts
+++ cypress.config.ts
@@ -26,12 +26,15 @@ export default defineConfig({
       // Make sure to return the config object as it might have been modified by the plugin.
       return config;
     },
+
     env: {
       filterSpecs: true,
-      // Without this command below, it will skip other tests so it is causing our test execution to be slow
+      // Without this command below, it will skip other tests so it is causing our test execution to be blow
       // So adding this command below will completely ignore the tests other than our @tag defined in the CLI
       omitFiltered: true,
     },
+
+    // pretty plugin output to get readable terminal results
     reporter: require.resolve("@badeball/cypress-cucumber-preprocessor/pretty-reporter")
   },
 });
